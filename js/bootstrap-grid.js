92e57468c23f9aa8613740d7876c176236a2b23b
James Paterson <j@presstube.com>
2012-12-12 10:04:37 -0500

    cleaning up

--- js/bootstrap-grid.js
+++ js/bootstrap-grid.js
@@ -1,24 +1,26 @@
+
 angular.module('bootstrapGrid', []).
 
   directive('bootstrapGrid', function() {
     return {
-
       scope: {items: '='},
       templateUrl: 'tpl/bootstrap-grid.html',
       transclude: true,
       replace: true,
-
       link: function(scope, iElement, iAttrs) {
-
-        var parentSpanWidth = iAttrs.parentSpanWidth || 12;
-        scope.spanWidth = iAttrs.spanWidth || 3;
-        scope.rows = makeRows();
-
-        console.log("scope.spanWidth", scope.spanWidth);
-
-        function makeRows() {
-          var itemsCopy = scope.items.slice(0),
-              itemsPerRow = parentSpanWidth / scope.spanWidth,
+        // throwing a useful error if 'items' array not passed in
+        scope.items = scope.items || function(){ throw "bootsrap-grid needs a valid 'items' attribute! It should look something like this: <div bootstrap-grid items='myItemsArray'></div>";}();
+        // grabbing any attributes being passed in
+        scope.spanWidth = iAttrs.spanWidth;
+        scope.parentSpanWidth = iAttrs.parentSpanWidth;
+        // setting defaults for unassigned, incorrect, or out of bounds attributes
+        if (!(scope.spanWidth >= 1 && scope.spanWidth <= 12)) scope.spanWidth = 3;
+        if (!(scope.parentSpanWidth >= 1 && scope.parentSpanWidth <= 12)) scope.parentSpanWidth = 12;
+        // building rows and exposing them on the scope
+        scope.rows = makeRows(scope.items, scope.spanWidth, scope.parentSpanWidth);
+        function makeRows(items, spanWidth, parentSpanWidth) {
+          var itemsCopy =items.slice(0),
+              itemsPerRow = parentSpanWidth / spanWidth,
               rows = [];
           while (itemsCopy.length) {
             rows.push(itemsCopy.splice(0, itemsPerRow));
@@ -28,26 +30,3 @@ angular.module('bootstrapGrid', []).
       }
     };
   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // template:
-    //   '<div>' +
-    //     '<div class="row" ng-repeat="row in rows">' +
-    //       '<div class="span{{spanWidth}}" ng-repeat="item in row">' +
-    //         '<div ng-transclude></div>' +
-    //       '</div>' +
-    //     '</div>' +
-    //   '</div>',
--- test/bootstrap-grid-spec.js
+++ test/bootstrap-grid-spec.js
@@ -1,22 +1,10 @@
 
-
-// what about loading in html scenarios?
-// use case scenarios?
-
-// var scenarios = [
-//   'default.html',
-//   'onlySpanWidth.html',
-//   'onlyParentSpanWidth.html'
-// ];
-
-// if I want to simulate different html use cases do I have to copy this whole describe block?
-
 describe('bootstrapGrid', function() {
   var elm, scope;
   beforeEach(module('bootstrapGrid'));
   beforeEach(module('tpl/bootstrap-grid.html'));
 
-  describe('scenario using all optional attributes', function() {
+  describe('scenario using all optional attributes correctly', function() {
 
     beforeEach(inject(function($rootScope, $compile) {
       elm = angular.element(
@@ -33,7 +21,7 @@ describe('bootstrapGrid', function() {
 
     it('should populate the correct number of rows and spans based on number of items, spanWidth, and parent-span-width', function() {
 
-      expect(elm.find('div.row').length).toBe(6); 
+      expect(elm.find('div.row').length).toBe(6);
       expect(elm.find('div.span4').length).toBeTruthy();
 
     });
@@ -59,36 +47,36 @@ describe('bootstrapGrid', function() {
     });
 
     it('should set its parentSpanWidth to a default of 12 if none is set', function() {
-      expect(elm.find('div.row').length).toBe(3); // 11(items) / (12(rowWidth) / 3(spanWidth)
-      dump(elm.find('div.row')[2]);
+      expect(elm.find('div.row').length).toBe(3);
     });
   }); 
 
-  // it should default its 'spanWidth' to 3 if a 'spanWidth' under 1 or over 12 is passed in
+  describe('scenario using out of bounds attributes', function() {
 
-  // it should default its 'parentSpanWidth' to 12 if no 'parentSpanWidth' is passed in && it is not a child of another bootstrap span
+    beforeEach(inject(function($rootScope, $compile) {
+      elm = angular.element(
+        '<div bootstrap-grid items="myTestItems" span-width="-2" parent-span-width="27">' +
+          '<div>' +
+            'hello {{item}}' +
+          '</div>' +
+        '</div>');
+      scope = $rootScope;
+      scope.myTestItems = ['item 1', 'item 2','item 3','item 4','item 5','item 6','item 7','item 8','item 9','item 10','item 11'];
+      $compile(elm)(scope);
+      scope.$digest();
+    }));
+
+    it("it should default its 'spanWidth' to 3 if 'spanWidth' passed in is out of bounds (1-12)", function() {
+      expect(elm.find('div.span3').length).toBeTruthy();
+    });
+
+    it("it should default its 'parentSpanWidth' to 12 if 'parentSpanWidth' passed in is out of bounds (1-12)", function() {
+      expect(elm.find('div.row').length).toBe(3);
+    });
+  }); 
+
+  // potential extra feature
 
   // it should set its 'parentSpanWidth' to the 'spanWidth' of a parent span in the DOM if it finds one and no valid 'parentSpanWidth' is excplicitly passed in
 
 });
-
-
-
-// describe('grid controller', function() {
-//   var scope, ctrl;
-//   beforeEach(module('bootstrapGrid'));
-
-//   beforeEach(inject(function($rootScope, $controller){
-//     scope = $rootScope;
-//     scope.spanWidth = 3;
-//     scope.parentSpanWidth = 12;
-//     scope.items = ['item 1', 'item 2','item 3','item 4','item 5','item 6','item 7','item 8','item 9','item 10','item 11'];
-//     ctrl = $controller('BootstrapGridCtrl', {$scope: scope});
-//   }));
-
-//   describe('constructor', function() {
-//     it('should chunk its items into an appropriate number of rows', function() {
-//       expect(scope.rows.length).toBe(3);
-//     });
-//   });
-// });
